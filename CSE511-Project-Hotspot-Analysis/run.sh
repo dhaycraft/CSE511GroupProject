df1d497bc0036a7efc9dae342d1283faf0ebc79f
dhaycraft <haycr009@gmail.com>
2021-07-04 03:15:58 -0500

    update path run.sh

--- CSE511-Project-Hotspot-Analysis/run.sh
+++ CSE511-Project-Hotspot-Analysis/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source ~/.profile
 sbt clean assembly
-spark-submit CSE511-Project-Hotspot-Analysis/target/scala-2.11/CSE512-Hotspot-Analysis-Template-assembly-0.1.0.jar test/output hotzoneanalysis src/resources/point_hotzone.csv src/resources/zone-hotzone.csv hotcellanalysis src/resources/yellow_trip_sample_100000.csv
+spark-submit target/scala-2.11/CSE512-Hotspot-Analysis-Template-assembly-0.1.0.jar test/output hotzoneanalysis src/resources/point_hotzone.csv src/resources/zone-hotzone.csv hotcellanalysis src/resources/yellow_trip_sample_100000.csv
